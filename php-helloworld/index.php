7eca8d01ed37b6182ef5460b2890df11b648e8db
Student User <student@workstation.lab.example.com>
2022-01-04 15:49:49 -0500

    changes

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,7 +1,3 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming!\n";
-print "Cause we're moving on up!\n";
-print "cha cha cha CHANGES!\n";
-print "indeed.....\n";
 ?>
